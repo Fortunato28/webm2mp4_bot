5642b00e233608b02f3e82f95601b496123bddc5
Anton Sapunov <fort.sav.28@gmail.com>
2020-04-20 02:16:46 +0700

    Teloxide added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world, test!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+
+    run().await;
+}
+
+async fn run()
+{
+teloxide::enable_logging!();
+    log::info!("Starting ping_pong_bot!");
+
+    let bot = Bot::from_env();
+
+    Dispatcher::new(bot)
+        .messages_handler(|rx: DispatcherHandlerRx<Message>| {
+            rx.for_each(|message| async move {
+                message.answer("pong").send().await.log_on_error().await;
+            })
+        })
+        .dispatch()
+        .await;
 }
