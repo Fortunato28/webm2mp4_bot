3727b7124d95fc7270edaa01254116151a4bd01d
Anton Sapunov <fort.sav.28@gmail.com>
2020-04-26 19:39:24 +0700

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,52 @@
 use teloxide::prelude::*;
 use std::fs::File;
 use std::io::Write;
+use bytes::Bytes;
 
-pub async fn download_resource(link: &str)
+pub struct Video
+{
+    pub filename: String,
+    body: Bytes,
+}
+
+impl Video
+{
+    pub async fn new(link: &str) -> Video {
+        let response = download_resource(link).await;
+        let filename = Self::get_filename(&response).await;
+        let body = Self::get_body(response).await;
+        Video {
+            filename,
+            body,
+        }
+    }
+
+    async fn get_body(response: reqwest::Response) -> Bytes
+    {
+        let body = response.bytes().await.expect("Problem while getting response body");
+        body
+    }
+
+    async fn get_filename(response: &reqwest::Response) -> String {
+        let filename = response
+                .url()
+                .path_segments()
+                .and_then(|segments| segments.last())
+                .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                .unwrap_or("tmp.bin")
+                .to_owned();
+
+        filename
+    }
+}
+
+pub async fn download_resource(link: &str) -> reqwest::Response
 {
     let client = reqwest::Client::new();
     let response = client.get(link).send().await.expect("Problem while GET request");
-    let body = response.bytes().await.expect("Problem while getting response body");
+    response
 
-    write_to_file("test", &body);
+    ////write_to_file(&filename, &body);
 }
 
 fn write_to_file(filename: &str, body: &[u8])
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use webm2mp4_bot as w2m;
 #[tokio::main]
 async fn main() {
     let link = "https://img-9gag-fun.9cache.com/photo/a1WpNrP_460svvp9.webm";
-    w2m::download_resource(link).await;
+
+    let video = w2m::Video::new(link).await;
+    dbg!(&video.filename);
+
+    //w2m::download_resource(link).await;
     //w2m::run().await;
 }
