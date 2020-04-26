a3221f2a314049560710a3a2ddf864c7acfe5784
Anton Sapunov <fort.sav.28@gmail.com>
2020-04-26 14:07:03 +0700

    Ping-pong

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use teloxide::prelude::*;
+
+use std::io::copy;
+use std::fs::File;
+use tempfile::Builder;
+
+pub async fn run() {
+teloxide::enable_logging!();
+    log::info!("Starting ping_pong_bot!");
+
+    let bot = Bot::from_env();
+
+    Dispatcher::new(bot)
+        .messages_handler(|rx: DispatcherHandlerRx<Message>| {
+            rx.for_each(|message| async move {
+                message.answer("pong").send().await.log_on_error().await;
+            })
+        })
+        .dispatch()
+        .await;
+}
