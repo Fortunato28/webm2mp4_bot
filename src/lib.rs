bf10cd140d46a60ed84c75352c50563bf2031b14
Anton Sapunov <fort.sav.28@gmail.com>
2020-04-26 20:09:47 +0700

    Save to directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,59 @@
 use teloxide::prelude::*;
 use std::fs::File;
+use std::fs;
 use std::io::Write;
 use bytes::Bytes;
 
-pub struct Video
-{
+pub struct Video {
     pub filename: String,
     body: Bytes,
 }
 
-impl Video
-{
+impl Video {
     pub async fn new(link: &str) -> Video {
-        let response = download_resource(link).await;
-        let filename = Self::get_filename(&response).await;
+        let response = Self::download_resource(link).await;
+        let filename = Self::get_filename(&response);
         let body = Self::get_body(response).await;
+
         Video {
             filename,
             body,
         }
     }
 
-    async fn get_body(response: reqwest::Response) -> Bytes
-    {
+    pub fn save_to_fs(&self, folder: &str) {
+        fs::create_dir(&folder);
+        let mut  full_filename = String::from(folder);
+        full_filename.push_str(&self.filename);
+        dbg!(&full_filename);
+        write_to_file(&full_filename, &self.body);
+    }
+
+    async fn get_body(response: reqwest::Response) -> Bytes {
         let body = response.bytes().await.expect("Problem while getting response body");
         body
     }
 
-    async fn get_filename(response: &reqwest::Response) -> String {
-        let filename = response
-                .url()
-                .path_segments()
-                .and_then(|segments| segments.last())
-                .and_then(|name| if name.is_empty() { None } else { Some(name) })
-                .unwrap_or("tmp.bin")
-                .to_owned();
-
-        filename
+    fn get_filename(response: &reqwest::Response) -> String {
+        response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin")
+            .to_owned()
     }
-}
 
-pub async fn download_resource(link: &str) -> reqwest::Response
-{
-    let client = reqwest::Client::new();
-    let response = client.get(link).send().await.expect("Problem while GET request");
-    response
+    async fn download_resource(link: &str) -> reqwest::Response {
+        let client = reqwest::Client::new();
+        client.get(link).send().await.expect("Problem while GET request")
+    }
 
-    ////write_to_file(&filename, &body);
 }
 
 fn write_to_file(filename: &str, body: &[u8])
 {
-    let mut destination = File::create(filename).expect("Problem while create  file");
+    let mut destination = File::create(filename).expect("Problem while create file");
     destination.write_all(body);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use webm2mp4_bot as w2m;
 #[tokio::main]
 async fn main() {
     let link = "https://img-9gag-fun.9cache.com/photo/a1WpNrP_460svvp9.webm";
-
+    let temp_dir = "tmp/";
     let video = w2m::Video::new(link).await;
-    dbg!(&video.filename);
-
-    //w2m::download_resource(link).await;
-    //w2m::run().await;
+    video.save_to_fs(temp_dir);
 }
